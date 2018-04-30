9876b41fcd6ee82f7262dc42284b7650ad575fde
pedro95gomes <pedro1995gomes@gmail.com>
2018-04-30 15:29:57 +0100

    script to compile and exec server

---
+++ HopOnCMU_server/src/compile.sh
@@ -0,0 +1,2 @@
+javac pt/ulisboa/tecnico/cmu/command/*.java pt/ulisboa/tecnico/cmu/domain/*.java pt/ulisboa/tecnico/cmu/response/*.java pt/ulisboa/tecnico/cmu/server/*.java
+java pt.ulisboa.tecnico.cmu.server.Server
